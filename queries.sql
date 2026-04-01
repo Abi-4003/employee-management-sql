7b9f491e1dc1ec020e63eea148cb281aa5d6cb6b
A.Abi <113366663+Abi-4003@users.noreply.github.com>
2026-04-01 21:27:38 +0530

    queries.sql

--- queries.sql
+++ queries.sql
@@ -35,4 +35,42 @@ JOIN Department d
 ON e.dept_id = d.dept_id;
 
 -- View data
+SELECT * FROM emp_details;  
+-- View all employees
+SELECT * FROM Employee;
+
+-- Join query
+SELECT e.emp_name, d.dept_name, e.salary
+FROM Employee e
+JOIN Department d
+ON e.dept_id = d.dept_id;
+
+-- Highest salary
+SELECT emp_name, salary
+FROM Employee
+WHERE salary = (SELECT MAX(salary) FROM Employee);
+
+-- Second highest salary
+SELECT MAX(salary)
+FROM Employee
+WHERE salary < (SELECT MAX(salary) FROM Employee);
+
+-- Average salary per department
+SELECT dept_id, AVG(salary)
+FROM Employee
+GROUP BY dept_id;
+
+-- Employees above average salary
+SELECT emp_name, salary
+FROM Employee
+WHERE salary > (SELECT AVG(salary) FROM Employee);
+
+-- Create View
+CREATE VIEW emp_details AS
+SELECT e.emp_name, d.dept_name, e.salary
+FROM Employee e
+JOIN Department d
+ON e.dept_id = d.dept_id;
+
+-- View data
 SELECT * FROM emp_details;
